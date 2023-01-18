8ff73f85aad2582110edeb2fc1dae894985f0fd7
Saad Javaid Alvi <saadjavaidalvi@gmail.com>
2023-01-19 00:16:29 +0500

    updated color field

--- lib/global/colors/my_colors.dart
+++ lib/global/colors/my_colors.dart
@@ -4,4 +4,6 @@ class MyColors {
   Color grey = const Color(0xff717171);
   Color black = Colors.black;
   Color white = Colors.white;
+  Color screenBg = const Color(0xffF8F8F8);
+  Color blue = const Color(0xff5286EA);
 }
